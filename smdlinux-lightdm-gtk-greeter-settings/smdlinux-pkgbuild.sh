6bd8e47a701d36c180b1bf6f9aecfe40fc14b9d0
Seth Dawson <seth.dawson@hotmail.com>
2021-01-27 20:22:09 -0600

    updated

--- smdlinux-lightdm-gtk-greeter-settings/smdlinux-pkgbuild.sh
+++ smdlinux-lightdm-gtk-greeter-settings/smdlinux-pkgbuild.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #https://wiki.archlinux.org/index.php/DeveloperWiki:Building_in_a_Clean_Chroot
 
-destination1=$HOME"/SMDLINUX-REPO/smdlinux_repo/x86_64/"
-destination2=$HOME"/SMDLINUX-REPO/smdlinux_repo_3party/x86_64/"
-destination3=$HOME"/SMDLINUX-REPO/smdlinux_repo_testing/x86_64/"
+destination1=$HOME"/SMDLINUX/SMDLINUX-REPO/smdlinux_repo/x86_64/"
+destination2=$HOME"/SMDLINUX/SMDLINUX-REPO/smdlinux_repo_3party/x86_64/"
+destination3=$HOME"/SMDLINUX/SMDLINUX-REPO/smdlinux_repo_iso/x86_64/"
+destination4=$HOME"/SMDLINUX/SMDLINUX-REPO/smdlinux_repo_testing/x86_64/"
+destination5=$HOME"/SMDLINUX/SMDLINUX-REPO/smdlinux_repo_xlarge/x86_64/"
+destination6=$HOME"/SMDLINUX/TEST/"
 
 destiny=$destination1
 
@@ -15,7 +18,7 @@ pwdpath=$(echo $PWD)
 pwd=$(basename "$PWD")
 
 #which packages are always going to be build with makepkg or choice 2
-makepkglist="arcolinux-dwm-slstatus-git arcolinux-dwm-st-git arcolinux-conky-collection-git arcolinux-conky-collection-plasma-git arco-dwm arcolinux-betterlockscreen arcolinux-lightdm-gtk-greeter smdlinux-lightdm-gtk-greeter-plasma arcolinux-logout-git arcolinux-logout-themes-git arcolinux-qobbar-git arcolinux-teamviewer arcolinux-tweak-tool-dev-git arcolinux-tweak-tool-git arco-dwm"
+makepkglist="smdlinux-dwm-slstatus-git smdlinux-dwm-st-git smdlinux-conky-collection-git smdlinux-conky-collection-plasma-git arco-dwm smdlinux-betterlockscreen smdlinux-lightdm-gtk-greeter smdlinux-lightdm-gtk-greeter-plasma smdlinux-logout-git smdlinux-logout-themes-git smdlinux-qobbar-git smdlinux-teamviewer smdlinux-tweak-tool-dev-git smdlinux-tweak-tool-git arco-dwm"
 
 for i in $makepkglist
 do
